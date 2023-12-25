76e50b1094f60e61d454645b275c0c3e29a2932b
Maksim Manylov <manylov@gmail.com>
2023-12-25 13:18:32 +0600

    experiment-3

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,30 @@
-#include <nil/crypto3/hash/algorithm/hash.hpp>
-#include <nil/crypto3/hash/sha2.hpp>
+#include <cstdint>
+#include <nil/crypto3/algebra/curves/pallas.hpp>
 
 using namespace nil::crypto3;
 
-bool is_same(typename hashes::sha2<256>::block_type block0,
-    typename hashes::sha2<256>::block_type block1){
+using field_type = algebra::curves::pallas::base_field_type;
 
-    return block0[0] == block1[0] && block0[1] == block1[1];
-}
+constexpr std::size_t VALIDATORS_COUNT = 1000000;
 
-[[circuit]] bool
-    validate_path(std::array<typename hashes::sha2<256>::block_type, 0x05> merkle_path,
-        typename hashes::sha2<256>::block_type leave,
-        typename hashes::sha2<256>::block_type root) {
-    
-    typename hashes::sha2<256>::block_type subroot = leave;
+[[circuit]] bool is_unique(
+    [[private_input]] size_t actual_validator_count,
+    [[private_input]] std::array<field_type::value_type, VALIDATORS_COUNT> sorted_validators_pubkeys,
+    [[private_input]] std::array<field_type::value_type, VALIDATORS_COUNT> initial_validators_pubkeys,
+    [[private_input]] std::array<size_t, VALIDATORS_COUNT> unique_to_initial_mapping)
+{
 
-    for (int i = 0; i < 0x05; i++) {
-        subroot = hash<hashes::sha2<256>>(subroot, merkle_path[i]);
+    // we check that sorted_validators_pubkeys are unique
+    // and each of shuffled element has same pubkey in initial_validators_pubkeys
+    // hence we know that initial_validators_pubkeys is unique
+
+    for (size_t i = 0; i < actual_validator_count - 1; ++i)
+    {
+        __builtin_assigner_exit_check(sorted_validators_pubkeys[i] < sorted_validators_pubkeys[i + 1]);
+        __builtin_assigner_exit_check(initial_validators_pubkeys[unique_to_initial_mapping[i]] == sorted_validators_pubkeys[i]);
     }
 
-    return is_same(subroot, root);
+    __builtin_assigner_exit_check(initial_validators_pubkeys[unique_to_initial_mapping[actual_validator_count - 1]] == sorted_validators_pubkeys[actual_validator_count - 1]);
+
+    return true;
 }
